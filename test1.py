f13e8f2c47a628f69056cc81101d239ee7c0e787
DeeJayBarti <borowski.bartek@wp.pl>
2017-06-07 19:16:40 +0200

    Add files via upload

--- test1.py
+++ test1.py
@@ -33,7 +33,7 @@ Podaj wynik: ", padx = 5, pady = 5)
 
 	#Przycisk "DALEJ"
 	przycisk_dalej = Button(okno, text = "Dalej", width = 6, 		height = 1)
-	przycisk_dalej.bind("<Button-1>")
+	przycisk_dalej.bind("<Button-1>", wybierz_ile)
 	przycisk_dalej.pack(side = BOTTOM, padx = 10, pady = 10)
 
 	okno.mainloop()
@@ -65,7 +65,7 @@ Podaj wynik: ", padx = 5, pady = 5)
 
 	#Przycisk "DALEJ"
 	przycisk_dalej = Button(okno, text = "Dalej", width = 6, 		height = 1)
-	przycisk_dalej.bind("<Button-1>")
+	przycisk_dalej.bind("<Button-1>", wybierz_ile)
 	przycisk_dalej.pack(side = BOTTOM, padx = 10, pady = 10)
 
 	okno.mainloop()
@@ -79,7 +79,7 @@ def brzuszki(zdarzenie):
 	etykieta = Label(okno, text = "By prawidłowo wykonać\n\
  brzuszki: \n\
 \n\
-* Połóż się na plecach\n\
+* Połóż się na plecach !\n\
 * Ugnij nogi w kolanach !\n\
 * Umieść dłonie za głową !\n\
  (Nie splatach ich, nie chwytaj głowy)\n\
@@ -87,16 +87,63 @@ def brzuszki(zdarzenie):
 * Unieś łopatki kilka cm nad ziemią !\n\
 * Z wdechem opuść tułów !", padx = 5, pady = 5)
 	etykieta.pack(expand=NO)
-	etykieta = Label(okno, text = "Zrób jak najwięcej powtórzeń !\n\
-Podaj wynik: ", padx = 5, pady = 5)
+	etykieta = Label(okno, text = "Zrób jak najwięcej powtórzeń !", padx = 5, pady = 5)
 	etykieta.pack(expand=NO)
-	entry = Entry(okno)
-	entry.pack()
 
 	#Przycisk "DALEJ"
 	przycisk_dalej = Button(okno, text = "Dalej", width = 6, 		height = 1)
-	przycisk_dalej.bind("<Button-1>")
+	przycisk_dalej.bind("<Button-1>", wybierz_ile)
 	przycisk_dalej.pack(side = BOTTOM, padx = 10, pady = 10)
+
+	okno.mainloop()
+
+#----------------------------------------- ilość powtórzeń (wybierz)
+def wybierz_ile(zdarzenie):
+
+	okno = Tk()
+	okno.geometry("240x300")
+	etykieta = Label(okno, text = "Kliknij na swój wynik !", padx = 30, pady = 10)
+	etykieta.pack(expand=NO)
+
+	# 1 - 19
+	przycisk1_19 = Button(okno, text = "1 - 19 powtórzeń", width=16, height=1)
+	przycisk1_19.bind("<Button-1>")
+	przycisk1_19.pack(padx = 2, pady = 2)
+
+	# 20 - 39
+	przycisk20_39 = Button(okno, text = "20 - 39 powtórzeń", width=16, height=1)
+	przycisk20_39.bind("<Button-1>")
+	przycisk20_39.pack(padx = 2, pady = 2)
+
+	# 40 - 59
+	przycisk40_59 = Button(okno, text = "40 - 59 powtórzeń", width=16, height=1)
+	przycisk40_59.bind("<Button-1>")
+	przycisk40_59.pack(padx = 2, pady = 2)
+
+	# 60 - 79
+	przycisk60_79 = Button(okno, text = "60 - 79 powtórzeń", width=16, height=1)
+	przycisk60_79.bind("<Button-1>")
+	przycisk60_79.pack(padx = 2, pady = 2)
+
+	# 80 - 99
+	przycisk80_99 = Button(okno, text = "80 - 99 powtórzeń", width=16, height=1)
+	przycisk80_99.bind("<Button-1>")
+	przycisk80_99.pack(padx = 2, pady = 2)
+
+	# 100 - 119
+	przycisk100_119 = Button(okno, text = "100 - 119 powtórzeń", width=16, height=1)
+	przycisk100_119.bind("<Button-1>")
+	przycisk100_119.pack(padx = 2, pady = 2)
+
+	# 120 - 139
+	przycisk120_139 = Button(okno, text = "120 - 139 powtórzeń", width=16, height=1)
+	przycisk120_139.bind("<Button-1>")
+	przycisk120_139.pack(padx = 2, pady = 2)
+
+	# 140 +
+	przycisk140 = Button(okno, text = "Ponad 150 powtórzeń", width=16, height=1)
+	przycisk140.bind("<Button-1>")
+	przycisk140.pack(padx = 2, pady = 2)
 
 	okno.mainloop()
 #-------------------------------- Funkcja wybrania PODCIĄGNIĘĆ
@@ -124,11 +171,15 @@ Podaj wynik: ", padx = 5, pady = 5)
 
 	#Przycisk "DALEJ"
 	przycisk_dalej = Button(okno, text = "Dalej", width = 6, 		height = 1)
-	przycisk_dalej.bind("<Button-1>")
+	przycisk_dalej.bind("<Button-1>", wybierz_ile)
 	przycisk_dalej.pack(side = BOTTOM, padx = 10, pady = 10)
-
+	
 	okno.mainloop()
 #-------------------------------- Program startowy
+
+def wyjdz():
+	okno.quit()
+	okno.destroy()
 
 okno = Tk()
 okno.geometry("240x300")
